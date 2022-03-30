08bd1e8b62dba85d44c2857ad8ea2177f765e0b2
humastefan19 <36787065+humastefan19@users.noreply.github.com>
2022-03-30 10:10:19 +0300

    Logging Aspect

--- src/main/java/com/javaproject/storeapp/aspect/LoggingAspect.java
+++ src/main/java/com/javaproject/storeapp/aspect/LoggingAspect.java
@@ -1,26 +1,12 @@
 package com.javaproject.storeapp.aspect;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.javaproject.storeapp.entity.Audit;
-import com.javaproject.storeapp.repository.AuditRepository;
-import com.javaproject.storeapp.service.impl.AuditServiceImpl;
-import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.aspectj.lang.reflect.CodeSignature;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Aspect
