07d6bad040adc579e20ece9776bdef7ecf0c9e4c
larisaandr97 <androniclarisa97@yahoo.com>
2022-05-05 15:39:30 +0300

    Added audit to ExceptionAspect

--- src/main/java/com/javaproject/storeapp/aspect/ExceptionAspect.java
+++ src/main/java/com/javaproject/storeapp/aspect/ExceptionAspect.java
@@ -1,5 +1,8 @@
 package com.javaproject.storeapp.aspect;
 
+import com.javaproject.storeapp.entity.Audit;
+import com.javaproject.storeapp.entity.AuditCategory;
+import com.javaproject.storeapp.repository.AuditRepository;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
@@ -8,18 +11,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Aspect
 @Component
 public class ExceptionAspect {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private final AuditRepository auditRepository;
+
+    public ExceptionAspect(AuditRepository auditRepository) {
+        this.auditRepository = auditRepository;
+    }
+
     @AfterThrowing("execution(* com.javaproject.storeapp.service.impl.OrderServiceImpl.findOrderById(..))")
     public void afterThrowingFindOrderById() {
+        final Audit audit = new Audit("findOrderById",
+                this.getClass().getCanonicalName(),
+                LocalDateTime.now(), AuditCategory.ERROR);
+        auditRepository.save(audit);
         this.logger.info("An exception was thrown inside findOrderById from OrderService ");
     }
 
     @AfterThrowing("execution(* com.javaproject.storeapp.service.impl.BankAccountServiceImpl.createBankAccount(..))")
     public void afterThrowing() {
+
+        final Audit audit = new Audit("createBankAccount",
+                this.getClass().getCanonicalName(),
+                LocalDateTime.now(), AuditCategory.ERROR);
+        auditRepository.save(audit);
         this.logger.info("A bank account with this number was already added! ");
     }
 
@@ -29,6 +49,10 @@ public class ExceptionAspect {
         Object result = null;
         try {
             result = joinPoint.proceed();
+            final Audit audit = new Audit(joinPoint.getSignature().getName(),
+                    joinPoint.getTarget().getClass().getCanonicalName(),
+                    LocalDateTime.now(), AuditCategory.INFO);
+            auditRepository.save(audit);
             this.logger.info("execution proceeded");
         } catch (Throwable throwable) {
             throwable.printStackTrace();
